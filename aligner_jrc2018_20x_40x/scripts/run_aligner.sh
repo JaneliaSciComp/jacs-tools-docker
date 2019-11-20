a88083bac0f794e5dc77fb8a00037fda54270e13
Cristian Goina <goinac@hhmi.org>
2019-11-20 10:26:39 -0500

    use xvfb

--- aligner_jrc2018_20x_40x/scripts/run_aligner.sh
+++ aligner_jrc2018_20x_40x/scripts/run_aligner.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DIR=$(cd "$(dirname "$0")"; pwd)
+
 area=
 gender=
 image_size=
@@ -12,8 +14,25 @@ objective=
 template_dirname=
 input_filepath=
 output_dir=
+start_display_port=10000
+shape=
 
-help_cmd="$0 --area <area>"
+help_cmd="$0 
+    --area <area>
+    --gender <gender>
+    --isize <image size>
+    --vsize <voxel size (defaults to 0.44x0.44x0.44)>
+    --mprotocol <mounting protocol>
+    --nmask <neuron mask>
+    --nchannels <number of channels (default = 4)>
+    --refchannel <reference channel (default = 4)>
+    --objective <objective>
+    --templatedir <template config directory>
+    --shape <Brain shape - Valid values: {Intact, Both_OL_missing (40x), Unknown}>
+    -i <input file stack>
+    -o <output directory>
+    --display-port <start display port>
+    -h"
 while [[ $# > 0 ]]; do
     key="$1"
     if [ "$key" == "" ] ; then
@@ -57,8 +76,12 @@ while [[ $# > 0 ]]; do
             objective=$1
             shift # past value
             ;;
-        --template)
+        --templatedir)
             template_dirname=$1
+            shift # past value
+            ;;
+        --shape)
+            shape=$1
             shift # past value
             ;;
         -i|--input)
@@ -67,6 +90,10 @@ while [[ $# > 0 ]]; do
             ;;
         -o|--output)
             output_dir=$1
+            shift # past value
+            ;;
+        --display-port)
+            start_display_port=$1
             shift # past value
             ;;
         -h|--help)
@@ -80,7 +107,33 @@ while [[ $# > 0 ]]; do
     esac
 done
 
-cat >${output_dir}/align.yml <<EOL
+WORKING_DIR=${output_dir}/temp
+
+function cleanTemp {
+    if [[ ${DEBUG_MODE} =~ "debug" ]]; then
+        echo "~ Debugging mode - Leaving temp directory"
+    else
+        echo "Cleaning ${WORKING_DIR}"
+        rm -rf ${WORKING_DIR}
+        echo "Cleaned up ${WORKING_DIR}"
+    fi
+}
+
+mkdir -p ${output_dir}
+
+if [[ $FB_MODE =~ "xvfb" ]]; then
+    echo "initialize virtual framebuffer"
+    . $DIR/init_xvfb.sh ${start_display_port} ${output_dir}
+    function exitHandler() { cleanXvfb; cleanTemp; }
+    trap exitHandler EXIT
+else
+    function exitHandler() { cleanTemp; }
+    trap exitHandler EXIT
+fi
+
+YAML_CONFIG_FILE=${output_dir}/align.yml
+
+cat > ${YAML_CONFIG_FILE} <<EOL
 template_dir: ${template_dirname}
 inputs:
 - area: ${area}
@@ -94,3 +147,6 @@ inputs:
   ref_channel: ${ref_channel}
   voxel_size: ${voxel_size}
 EOL
+
+
+/opt/aligner/20xBrain_Align_CMTK.sh ${YAML_CONFIG_FILE} ${output_dir} ${shape}
