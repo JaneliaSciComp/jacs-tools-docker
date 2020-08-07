c6a9fdfccd7fb35696514c10fcb6d6d46443a05c
Cristian Goina <goinac@hhmi.org>
2020-08-07 17:45:06 -0400

    reformat neuronbridge alignment script

--- aligner_jrc2018_20x_40x/build.sh
+++ aligner_jrc2018_20x_40x/build.sh
@@ -1,3 +1,1 @@
 docker build . -t registry.int.janelia.org/jacs-scripts/aligner_jrc2018_20x_40x:1.0
-#docker push nexus.janelia.org/docker-hosted/jacs-tools/aligner_jrc2018_20x_40x:1.0
-#docker push registry.int.janelia.org/jacs-scripts/aligner_jrc2018_20x_40x:1.0
