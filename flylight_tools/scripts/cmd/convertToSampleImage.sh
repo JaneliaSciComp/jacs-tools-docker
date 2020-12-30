86f207852e5981154a551128b9150975b7ef2dcb
Konrad Rokicki <rokickik@janelia.hhmi.org>
2020-12-29 20:52:09 -0500

    fixed syntax

--- flylight_tools/scripts/cmd/convertToSampleImage.sh
+++ flylight_tools/scripts/cmd/convertToSampleImage.sh
@@ -4,28 +4,22 @@ INPUT_FILENAME=$1
 OUTPUT_FILENAME=$2
 CHANNEL_MAPPING=$3
 FLIP=$4
-SCRATCH_DIR=$5
 
-. /app/scripts/utils/legacy_init_xvfb.sh
+. /app/scripts/utils/initXvfb.sh
 
-if [ -z "$SCRATCH_DIR" ]; then
-    export TMPDIR=$SCRATCH_DIR
-    mkdir -p $TMPDIR
-fi
+echo "Using $TMPDIR as temporary root"
+mkdir -p $TMPDIR
 WORKING_DIR=`mktemp -d`
-echo "Working dir: $WORKING_DIR"
-
 function cleanTemp {
-    echo "Cleaning $WORKING_DIR"
     rm -rf $WORKING_DIR
     echo "Cleaned up $WORKING_DIR"
 }
 
-// Two EXIT handlers
+# Two EXIT handlers
 function exitHandler() { cleanXvfb; cleanTemp; }
 trap exitHandler EXIT
 
-// Go to the working directory
+# Go to the working directory
 cd $WORKING_DIR
 
 if [ "$FLIP" != "" ]; then
@@ -37,7 +31,6 @@ if [ "$FLIP" != "" ]; then
     else
         /app/scripts/utils/flipZ.sh "$INPUT_FILENAME" "$OUTPUT_FILENAME"
     fi
-	
 fi
 
 if [ "$CHANNEL_MAPPING" != "" ]; then
